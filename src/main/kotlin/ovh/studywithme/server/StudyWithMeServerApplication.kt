f30fd2ea92eede88da1af2e6c90421c3b872deeb
Vincent Wilke <vincent@wilkeimnetz.de>
2022-01-16 17:12:36 +0100

    Initial Commit

---
+++ src/main/kotlin/ovh/studywithme/server/StudyWithMeServerApplication.kt
@@ -0,0 +1,11 @@
+package ovh.studywithme.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyWithMeServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyWithMeServerApplication>(*args)
+}
