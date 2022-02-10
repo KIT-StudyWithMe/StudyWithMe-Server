c1597b37cc650acf2318e393b57931600558b0c7
Vincent Wilke <vincent@wilkeimnetz.de>
2022-02-10 22:13:25 +0100

    Document GroupRepository

--- src/main/kotlin/ovh/studywithme/server/repository/GroupRepository.kt
+++ src/main/kotlin/ovh/studywithme/server/repository/GroupRepository.kt
@@ -6,76 +6,81 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 /**
- * Group repository
+ * This Group repository is used to access the Group-Data in the Database in a generic way.
+ * Many functions are inherited from the JpaRepository. Even the functions declared below are automatically implemented.
  *
- * @constructor Create empty Group repository
+ * @constructor Create Group repository
  */
 @Repository
 interface GroupRepository : JpaRepository<StudyGroup, Long> {
 
     /**
-     * Find by name
+     * Find a Group in the database by name
      *
-     * @param name
-     * @return
+     * @param name The name of the Group
+     * @return A list of matching Groups
      */
     fun findByName(name:String): List<StudyGroup>
 
     /**
-     * Find by lecture i d
+     * Find a Group in the database by lecture-ID
      *
-     * @param lectureId
-     * @return
+     * @param lectureId The unique ID of a Lecture to search for
+     * @return A list of Groups that belong to that lecture
      */
     fun findByLectureID(lectureId:Long): List<StudyGroup>
 }
 
 /**
- * Group member repository
+ * This Group member repository is used to access the Group-Member-Data in the Database in a generic way.
+ * Many functions are inherited from the JpaRepository. Even the functions declared below are automatically implemented.
+ * Not all GroupMembers in the repository are participants in the group, some just requested the membership in the group.
  *
- * @constructor Create empty Group member repository
+ * @constructor Create Group-Member repository
  */
 @Repository
 interface GroupMemberRepository : JpaRepository<StudyGroupMember, Long> {
 
     /**
-     * Find by group i d
+     * Find Users that are related to the Group no matter if they requested the participation or if they are actual participants
      *
-     * @param groupID
-     * @return
+     * @param groupID Unique GroupID for which all GroupMembers should be returned
+     * @return All GroupMembers that are related to the specified GroupID
      */
     fun findByGroupID(groupID:Long): List<StudyGroupMember>
 
     /**
-     * Delete by group i d
+     * Delete all GroupMembers in a Group
      *
-     * @param groupID
+     * @param groupID GroupID for which all GroupMembers should be deleted
      */
     fun deleteByGroupID(groupID:Long)
 
     /**
-     * Find by group i d and user i d
+     * Find a unique GroupMember by its GroupID and its UserID
      *
-     * @param groupID
-     * @param userID
-     * @return
+     * @param groupID The GroupMembers GroupID
+     * @param userID The GroupMembers UserID
+     * @return The specified GroupMember in the Database
      */
     fun findByGroupIDAndUserID(groupID:Long, userID:Long): StudyGroupMember
 
     /**
-     * Exists by group i d and user i d
+     * Check if there is a GroupMember for the specified GroupID and UserID
      *
-     * @param groupID
-     * @param userID
-     * @return
+     * @param groupID GroupID of the GroupMember
+     * @param userID UserID of the GroupMember
+     * @return True if the specified GroupMember exists
      */
     fun existsByGroupIDAndUserID(groupID:Long, userID:Long): Boolean
 
     /**
-     * Delete by group i d and user i d
+     * Delete a GroupMember. 
+     * For a GroupMember that requested the membership this is equal to a refusal of that request.
+     * For a GroupMember that is in the Group that means the User gets removed from the Group
      *
-     * @param groupID
-     * @param userID
+     * @param groupID GroupID of the GroupMember
+     * @param userID UserID of the GroupMember
      */
     fun deleteByGroupIDAndUserID(groupID:Long, userID:Long)
 }
