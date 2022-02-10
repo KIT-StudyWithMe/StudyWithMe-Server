5f282c4c280173045a0306b90f52a02cb21dc8c0
Vincent Wilke <vincent@wilkeimnetz.de>
2022-02-10 16:16:28 +0100

    Add JavaDoc about UserController Variables

--- src/main/kotlin/ovh/studywithme/server/controller/UserControllerInterface.kt
+++ src/main/kotlin/ovh/studywithme/server/controller/UserControllerInterface.kt
@@ -14,7 +14,7 @@ interface UserControllerInterface {
     /**
      * Get all users. Returns an empty List if there are no Users.
      *
-     * @return
+     * @return List of all Users
      */
     fun getAllUsers():List<UserDAO>
 
@@ -22,8 +22,8 @@ interface UserControllerInterface {
      * Get detailed User information about the user with the specified ID
      * If there is no user with that ID, null is returned
      *
-     * @param userID
-     * @return
+     * @param userID UserID which details should be fetched
+     * @return Detailed User Information
      */
     fun getUserDetailByID(userID:Long):UserDetailDAO?
 
@@ -31,8 +31,8 @@ interface UserControllerInterface {
      * Get basic user information about the user with the specified ID
      * If there is no user with that ID, null is returned
      *
-     * @param userID
-     * @return
+     * @param userID UserID which details should be fetched
+     * @return Basic User Information
      */
     fun getUserByID(userID:Long):UserDAO?
 
@@ -41,8 +41,8 @@ interface UserControllerInterface {
      * If the user is not found null is returned.
      * If the user has no Groups an empty List is returned.
      *
-     * @param userID
-     * @return
+     * @param userID UserID of the User which groups should be returned
+     * @return List of all groups the user is in
      */
     fun getUsersGroups(userID:Long): List<StudyGroupDAO>?
 
@@ -50,8 +50,8 @@ interface UserControllerInterface {
      * Create a new User. You have to choose 0 as UserID.
      * The method returns the newly created user with the generated UserID
      *
-     * @param user
-     * @return
+     * @param user UserInformation with which the User should be created
+     * @return The User that was created
      */
     fun createUser(userDetailDAO:UserDetailDAO): UserDetailDAO
 
@@ -60,9 +60,9 @@ interface UserControllerInterface {
      * If there is no User with the userID then null is returned.
      * If it was successful the updated User is returned.
      *
-     * @param userID
-     * @param updatedUser
-     * @return
+     * @param userID The User ID of the user that should be updated
+     * @param updatedUser The new User-contents that the user should have
+     * @return The updated User with the new contents
      */
     fun updateUser(userID: Long, updatedUser:UserDetailDAO): UserDetailDAO?
 
@@ -71,8 +71,8 @@ interface UserControllerInterface {
      * If there is no User with that ID then false is returned.
      * If there is a user with that ID then it gets deleted and true is returned.
      *
-     * @param userID
-     * @return
+     * @param userID The userID of the user that should get deleted
+     * @return True if the user existed and is now deleted, False if the user didn't exist
      */
     fun deleteUser(userID:Long): Boolean
 
@@ -80,8 +80,8 @@ interface UserControllerInterface {
      * Searches in the Database for Users with that Firebase User ID.
      * It returns a List of matches. If no match is found an empty List is Returned
      *
-     * @param firebaseUID
-     * @return
+     * @param firebaseUID FirebaseUID to search for
+     * @return All Users that have exactly this FirebaseUID
      */
     fun getUserByFUID(firebaseUID:String): List<UserDAO>
 
@@ -90,10 +90,10 @@ interface UserControllerInterface {
      * If the userID, the reporterID and the field is found, then the Field gets reported and true is returned.
      * If one of them is not found false is returned.
      *
-     * @param userID
-     * @param reporterID
-     * @param field
-     * @return
+     * @param userID UserID of the user with the field that should be reported
+     * @param reporterID UserID of the user that creates the report
+     * @param field Field that should be reported
+     * @return True if the Field was reported, False if the Field was not found
      */
     fun reportUserField(userID:Long, reporterID:Long, field: UserField): Boolean
 
@@ -102,16 +102,16 @@ interface UserControllerInterface {
      * If the userID and the userID of the Moderator exist then the user gets blocked and true is returned.
      * If either of them is not found false is returned.
      *
-     * @param userID
-     * @param moderatorID
-     * @return
+     * @param userID UserID of the User which should get blocked
+     * @param moderatorID UserID of the User that is a Moderator and requests the blocking
+     * @return True if the User is now blocked, False if either of the two users was not found or had wrong permissions
      */
     fun blockUser(userID:Long, moderatorID:Long): Boolean
 
     /**
      * Returns a list of blocked Users.
      *
-     * @return
+     * @return List of all Users that are blocked
      */
     fun getBlockedUsers(): List<UserDAO>
 }
