f3390805a06033f99c318b0af5fba626e85d2021
Martin <martin.kaiser@student.kit.edu>
2022-03-11 03:07:45 +0100

    Implemented get all user reports test.

--- src/test/kotlin/ovh/studywithme/server/ReportTests.kt
+++ src/test/kotlin/ovh/studywithme/server/ReportTests.kt
@@ -17,10 +17,7 @@ import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.api.TestInstance
 import org.springframework.web.bind.annotation.PutMapping
 import ovh.studywithme.server.dao.*
-import ovh.studywithme.server.model.SessionFrequency
-import ovh.studywithme.server.model.SessionMode
-import ovh.studywithme.server.model.StudyGroupField
-import ovh.studywithme.server.model.StudyGroupReport
+import ovh.studywithme.server.model.*
 
 
 @ExtendWith(SpringExtension::class)
@@ -37,17 +34,6 @@ class ReportTests : RestTests() {
     @LocalServerPort
     var port: Int = 0
 
-    private var firstMajor = MajorDAO(0, "Informatik")
-
-    @BeforeAll
-    fun init() {
-        firstMajor = post("/majors", firstMajor, trt, port)
-    }
-
-    @AfterAll
-    fun tearDown() {
-        delete("/majors/${firstMajor.majorID}", trt, port)
-    }
 
     private fun createAGroup() : StudyGroupDAO {
         val inst = post<InstitutionDAO, InstitutionDAO>("/institutions", InstitutionDAO(0, "FHKA"), trt, port)
@@ -99,7 +85,7 @@ class ReportTests : RestTests() {
         val fetchedGroupReports = getEx("/reports/group", trt, port)
         val body : String? = fetchedGroupReports.body
         assertNotNull(body)
-        val parsedList:List<StudyGroupReport>? = body?.let { Klaxon().parseArray(it) }
+        val parsedList:List<StudyGroupReportDAO>? = body?.let { Klaxon().parseArray(it) }
 
         assertEquals(true, parsedList!!.map { it.reporterID }.contains(user1.userID))
         assertEquals(true, parsedList.map { it.reporterID }.contains(user2.userID))
@@ -107,151 +93,25 @@ class ReportTests : RestTests() {
     }
 
     @Test
-    fun `Create a new institution and request it`() {
-        val institutionData = InstitutionDAO(0, "KIT")
-        val newInstitution = post<InstitutionDAO, InstitutionDAO>("/institutions", institutionData, trt, port)
-        val fetchedInstitution = get<InstitutionDAO>("/institutions/" + newInstitution.institutionID, trt, port)
+    fun `Create user reports and verify the list of all user reports contains them`() {
+        // creating new users ensures their ID is brand new and not used by someone else
+        val user1 = post<UserDetailDAO, UserDetailDAO>("/users", createAUser(), trt, port)
+        val user2 = post<UserDetailDAO, UserDetailDAO>("/users", createAUser(), trt, port)
+        val user3 = post<UserDetailDAO, UserDetailDAO>("/users", createAUser(), trt, port)
 
-        assertEquals(newInstitution, fetchedInstitution)
-        assertEquals(institutionData.name, fetchedInstitution.name)
-        assertNotEquals(institutionData.institutionID, fetchedInstitution.institutionID)
-    }
+        // those don't return anything, hence the workaround with the reporterID later on when using contains
+        put<UserField, Void>("/users/${user2.userID}/report/${user1.userID}", UserField.CONTACT, trt, port)
+        put<UserField, Void>("/users/${user3.userID}/report/${user2.userID}", UserField.NAME, trt, port)
+        put<UserField, Void>("/users/${user1.userID}/report/${user3.userID}", UserField.MAJOR, trt, port)
+        put<UserField, Void>("/users/${user1.userID}/report/${user2.userID}", UserField.INSTITUTION, trt, port)
 
-    @Test
-    fun `Delete a created institution`() {
-        val institutionData = InstitutionDAO(0, "Fernuni Hagen")
-        val newInstitution = post<InstitutionDAO, InstitutionDAO>("/institutions", institutionData, trt, port)
-
-        delete("/institutions/${newInstitution.institutionID}", trt, port)
-        val result = getEx("/institutions/${newInstitution.institutionID}", trt, port)
-        val body = getBody(result)
-
-        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
-        assertEquals("", body)
-    }
-
-    @Test
-    fun `Delete a non-existing institution`() {
-        val result = deleteEx("/institutions/0", trt, port)
-
-        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
-    }
-
-    //###############
-    //# major tests #
-    //###############
-
-    @Test
-    fun `Get major with ID 0 and expect nothing`() {
-        val result = getEx("/majors/0", trt, port)
-        val body = getBody(result)
-
-        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
-        assertEquals("", body)
-    }
-
-    @Test
-    fun `Create some majors and verify the list of all majors contains them`() {
-        val newMajor1 = post<MajorDAO, MajorDAO>("/majors", MajorDAO(0, "Chemie"), trt, port)
-        val newMajor2 = post<MajorDAO, MajorDAO>("/majors", MajorDAO(0, "Gender Studies"), trt, port)
-
-        val fetchedInstitutions = getEx("/majors", trt, port)
-        val body : String? = fetchedInstitutions.body
+        val fetchedGroupReports = getEx("/reports/user", trt, port)
+        val body : String? = fetchedGroupReports.body
         assertNotNull(body)
-        val parsedList:List<MajorDAO>? = body?.let { Klaxon().parseArray(it) }
+        val parsedList:List<UserReportDAO>? = body?.let { Klaxon().parseArray(it) }
 
-        assertEquals(true, parsedList!!.contains(newMajor1))
-        assertEquals(true, parsedList.contains(newMajor2))
-    }
-
-    @Test
-    fun `Create a new major and request it`() {
-        val majorData = MajorDAO(0, "Informatik")
-        val newMajor = post<MajorDAO, MajorDAO>("/majors", majorData, trt, port)
-        val fetchedMajor = get<MajorDAO>("/majors/" + newMajor.majorID, trt, port)
-
-        assertEquals(newMajor, fetchedMajor)
-        assertEquals(majorData.name, fetchedMajor.name)
-        assertNotEquals(majorData.majorID, fetchedMajor.majorID)
-    }
-
-    @Test
-    fun `Delete a created major`() {
-        val majorData = MajorDAO(0, "Mandala Informatik")
-        val newMajor = post<MajorDAO, MajorDAO>("/majors", majorData, trt, port)
-
-        delete("/majors/${newMajor.majorID}", trt, port)
-        val result = getEx("/majors/${newMajor.majorID}", trt, port)
-        val body = getBody(result)
-
-        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
-        assertEquals("", body)
-    }
-
-    @Test
-    fun `Delete a non-existing major`() {
-        val result = deleteEx("/majors/0", trt, port)
-
-        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
-    }
-
-    //#################
-    //# lecture tests #
-    //#################
-
-    @Test
-    fun `Get lecture with ID 0 and expect nothing`() {
-        val result = getEx("/lectures/0", trt, port)
-        val body = getBody(result)
-
-        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
-        assertEquals("", body)
-    }
-
-    @Test
-    fun `Create some lectures and verify the list of all majors contains them`() {
-        val newMajor = post<MajorDAO, MajorDAO>("/majors", MajorDAO(0, "Physik"), trt, port)
-
-        val newLecture1 = post<LectureDAO, MajorDAO>("/majors/${newMajor.majorID}/lectures", LectureDAO(0, "Ex Physik III", newMajor.majorID), trt, port)
-        val newLecture2 = post<LectureDAO, MajorDAO>("/majors/${newMajor.majorID}/lectures", LectureDAO(0, "Theo Physik IV", newMajor.majorID), trt, port)
-
-        val fetchedInstitutions = getEx("/majors/${newMajor.majorID}/lectures", trt, port)
-        val body : String? = fetchedInstitutions.body
-        assertNotNull(body)
-        val parsedList:List<MajorDAO>? = body?.let { Klaxon().parseArray(it) }
-
-        assertEquals(true, parsedList!!.contains(newLecture1))
-        assertEquals(true, parsedList!!.contains(newLecture2))
-    }
-
-    @Test
-    fun `Create a new lecture and request it`() {
-        val lectureData = LectureDAO(0, "Softwaretechnik I", firstMajor.majorID)
-        val newLecture = post<LectureDAO, LectureDAO>("/majors/${firstMajor.majorID}/lectures", lectureData, trt, port)
-        val fetchedLecture = get<LectureDAO>("/lectures/" + newLecture.lectureID, trt, port)
-
-        assertEquals(newLecture, fetchedLecture)
-        assertEquals(lectureData.name, fetchedLecture.name)
-        assertNotEquals(lectureData.lectureID, fetchedLecture.lectureID)
-    }
-
-    @Test
-    fun `Delete a created lecture`() {
-        val lectureData = LectureDAO(0, "Programmieren I", firstMajor.majorID)
-        val newLecture = post<LectureDAO, LectureDAO>("/majors/${firstMajor.majorID}/lectures", lectureData, trt, port)
-
-        delete("/majors/${firstMajor.majorID}/lectures/${newLecture.lectureID}", trt, port)
-        val result = getEx("/lectures/${newLecture.lectureID}", trt, port)
-        val body = getBody(result)
-
-        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
-        assertEquals("", body)
-    }
-
-    @Test
-    fun `Delete a non-existing lecture`() {
-        val result = deleteEx("/majors/${firstMajor.majorID}/lectures/0", trt, port)
-
-        assertEquals(HttpStatus.NOT_FOUND, result.statusCode)
+        assertEquals(true, parsedList!!.map { it.reporterID }.contains(user1.userID))
+        assertEquals(true, parsedList.map { it.reporterID }.contains(user2.userID))
+        assertEquals(2, parsedList.map { it.reporterID }.filter { it == user2.userID }.size)
     }
 }
